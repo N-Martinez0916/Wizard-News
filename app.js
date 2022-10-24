3af75df9ff12a06fba87f3f6d036ca003453ee30
N-Martinez0916 <nicomartin0916@gmail.com>
2022-10-24 15:59:27 -0400

    added to route, and added postBank

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require("express");
-const app = express();
+
+const postBank = require('./postBank')
 
 const morgan= require('morgan')
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const PORT = 1337;
+
+const app = express();
+
 
 app.use(morgan('dev'))
 
+app.get("/", (req, res) => {
 
-const PORT = 1337;
+  const posts = postBank.list()
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+  <title> Wizard News </title>
+  </head>
+  <body>
+  <ul<
+  ${posts.map(post => `<li>${post.title, post.name}</li>`)}
+  </ul>
+  </body>
+  </html>`
+
+
+  res.send(html)
+})
+
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
